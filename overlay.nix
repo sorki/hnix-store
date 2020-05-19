d38a59773e40f6058776a85a3e8513989bddf3e3
Richard Marko <srk@48.io>
2020-05-19 19:11:39 +0200

    hnix-store-db: init

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,8 @@
 huper: helf: {
   hnix-store-core =
     helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
+  hnix-store-db =
+    helf.callCabal2nix "hnix-store-db" ./hnix-store-db {};
   hnix-store-remote =
     helf.callCabal2nixWithOptions "hnix-store-remote" ./hnix-store-remote "-fio-testsuite" {};
 }
